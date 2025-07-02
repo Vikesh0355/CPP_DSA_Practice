0edace053b1aaf7319af97d7546f95ee41d97d8e
Vikesh <vikesh@example.com>
2025-07-02 18:46:59 +0200

    added main function

--- src/Week1_Arrays_Strings/Two_Sum.cpp
+++ src/Week1_Arrays_Strings/Two_Sum.cpp
@@ -1,26 +1,47 @@
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
 class Solution
 {
- public:
-  vector<int> twoSum(vector<int>& nums, int target)
-  {
-    unordered_map<int, int> seen;
-    for(int i = 0; i<nums.size(); i++)
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
     {
-        int x = target-nums[i];
-        if(seen.find(x)!= seen.end())
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++)
         {
-            return {seen[x], i};
+            int x = target - nums[i];
+            if (seen.find(x) != seen.end())
+            {
+                return {seen[x], i};
+            }
+            seen[nums[i]] = i;
         }
-        seen[nums[i]] = i;
+        return {};
     }
-    return {};
-  }
-
 };
 
-/*  g++ -std=c++14 Two_Sum.cpp -o AbstTwo_SumractClass -pthread */
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty())
+    {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    }
+    else
+    {
+        cout << "No two numbers found that add up to the target." << endl;
+    }
+
+    return 0;
+}
+
+
+/*  g++ -std=c++14 Two_Sum.cpp -o Two_Sum -pthread */
 /* ./Two_Sum  */
