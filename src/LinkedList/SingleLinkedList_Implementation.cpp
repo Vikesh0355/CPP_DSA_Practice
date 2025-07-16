3ec9d37a972ad19c670e0f272bae01c7ff9e7518
Vikesh Kumar Mishra <mishravikesh95@.com>
2025-07-16 17:36:36 +0200

    added various insertion logic

--- src/LinkedList/SingleLinkedList_Implementation.cpp
+++ src/LinkedList/SingleLinkedList_Implementation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 struct node
 {
-    int data ;
+    int data;
     struct node* next;
 };
 
@@ -25,10 +25,8 @@ void create_SingleLinkedList(void)
             cout<<" memory creation failed\n"<<endl;
             
         }
-
         cout<<"Enter the data\n"<<endl;
         cin>>newnode->data;
-
         if(head== NULL)
         {
             head = temp = newnode;
@@ -38,15 +36,14 @@ void create_SingleLinkedList(void)
             temp->next = newnode;
             temp = newnode;
         }
-
         cout<<"do you want to continue: press 1 to conitnue : 0 for exit\n"<<endl;
         cin>>choice;
     }
 
 }
 
-void display()
-{
+ void display()
+ {
     temp = head;
     while(temp != NULL)
     {
@@ -54,12 +51,77 @@ void display()
         temp = temp->next;
     }
     cout<<endl;
-
-}
-
-int main()
-{
+ }
+ 
+ void insert_atbeginning(void)
+ {
+    newnode = (struct node*)malloc(sizeof(struct node));
+    if(newnode == NULL)
+    {
+        exit(1);
+    }
+    cout<<"Enter the data you want to insert at beginning\n"<<endl;
+    cin>>newnode->data;
+    temp = head;
+    head = newnode;
+    head->next = temp;
+    display();
+ }
+ 
+ void insert_atEnd(void)
+ {
+    newnode = (struct node*)malloc(sizeof(struct node));
+    if(newnode == NULL)
+    {
+        exit(1);
+    }
+    
+    cout<<"Enter the data you want to insert at end\n";
+    cin>>newnode->data;
+    newnode->next = NULL;
+    
+    temp = head; // assign temp to head for traversal to last
+    while(temp->next!=NULL)
+    {
+            
+            temp = temp->next; // moved the temp till last node;
+    }
+    temp->next = newnode; // now assigned temp->next to newnode
+    display();
+ }
+ 
+ void insert_atPosition()
+ {
+    int position, i = 1;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    if(newnode == NULL)
+    {
+         exit(1);
+    }
+     
+    cout<<"Enter the  new poition\n";
+    cin>>position;
+    cout<<"Enter the data for new position\n";
+    cin>>newnode->data;
+    newnode->next = NULL;
+        
+    temp = head; // assign temp to head for traversal to last
+    while ( i < position-1)
+    {
+        temp = temp->next;
+        i++;
+    }
+    newnode->next = temp->next; // in newnode->next assign nextnode
+    temp->next = newnode; // link the given postion node
+    display();
+ }
+ 
+ int main()
+ {
     create_SingleLinkedList();
     display();
+    insert_atbeginning();
+    insert_atEnd();
+    insert_atPosition();
     return 0;
-}
+ }
