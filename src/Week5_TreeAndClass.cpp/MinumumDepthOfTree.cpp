b5ac93b2648a7a58ebde6adb50e069043cc05b2d
Vikesh Kumar Mishra <mishravikesh95@.com>
2025-08-07 17:02:03 +0200

    added min Depth of tree

---
+++ src/Week5_TreeAndClass.cpp/MinumumDepthOfTree.cpp
@@ -0,0 +1,19 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int minDepth(TreeNode* root) {
+    if (!root) return 0;
+
+    // If left subtree is NULL, recur for right subtree
+    if (!root->left) return 1 + minDepth(root->right);
+
+    // If right subtree is NULL, recur for left subtree
+    if (!root->right) return 1 + minDepth(root->left);
+
+    // If both children exist
+    return 1 + std::min(minDepth(root->left), minDepth(root->right));
+}
